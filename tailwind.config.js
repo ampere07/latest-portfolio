b8a874a34a3899a0025dd846b8b8982bd4c4f02b
ampere07 <ravenampere0123@gmail.com>
2025-03-09 16:48:36 +0800

    a

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        background: 'var(--background)',
+      },
+      animation: {
+        'bounce-slow': 'bounce 3s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+};
